f2c8131bebc77afa97332146cfeb5895ce0f6691
ArshKarpoor <77087246+ArshKarpoor@users.noreply.github.com>
2021-01-07 10:49:13 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('header h2').addEventListener('click', function () {
+  window.open("file:///C:/Users/DELL/Desktop/Arsh%20Folder/Website%20made%20by%20arsh/Website%20Using%20Html%20&%20Css/index.html", "_self");
+});
+
+document .querySelector('.menu').addEventListener('')
