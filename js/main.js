2808c059777a8425acc120288e79686fa5b586f5
ArshKarpoor <77087246+ArshKarpoor@users.noreply.github.com>
2021-01-10 18:31:49 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,14 @@ document.querySelector('header h2').addEventListener('click', function () {
   window.open("file:///C:/Users/DELL/Desktop/Arsh%20Folder/Website%20made%20by%20arsh/Website%20Using%20Html%20&%20Css/index.html", "_self");
 });
 
-document .querySelector('.menu').addEventListener('')
+// Menu
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('header');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('show');
+  header.classList.toggle('toggled');
+  hamburger.classList.toggle('toggleIsOn');
+});
